6d0029174d4ce75bd8c314d2998998003a484213
Madsen Jacob <jacobma@l31.vsc3plus.xcat>
2019-09-09 11:37:58 +0200

    added mpi example

--- examples/parallel/export_potential.py
+++ examples/parallel/export_potential.py
@@ -4,16 +4,16 @@ from abtem.dft import GPAWPotential
 from abtem.potentials import import_potential
 import matplotlib.pyplot as plt
 
-#atoms = read('../data/graphene.traj')
-#atoms.center(vacuum=4, axis=2)
-#atoms *= (2, 1, 1)
+atoms = read('../data/graphene.traj')
+atoms.center(vacuum=4, axis=2)
+atoms *= (2, 1, 1)
 
-#calc = GPAW(mode=PW(600), eigensolver='cg', h=.2, txt=None)
-#atoms.set_calculator(calc)
-#atoms.get_potential_energy()
-#calc.write('graphene.gpw')
+calc = GPAW(mode=PW(600), eigensolver='cg', h=.2, txt=None)
+atoms.set_calculator(calc)
+atoms.get_potential_energy()
+calc.write('graphene.gpw')
 
-calc = GPAW('graphene.gpw', txt=None)
+#calc = GPAW('graphene.gpw', txt=None)
 
 potential = GPAWPotential(calc, sampling=.015, slice_thickness=.4).precalculate()
 potential.export('potential.npz', overwrite=True)
--- examples/parallel/mpi_export.py
+++ examples/parallel/mpi_export.py
@@ -7,21 +7,26 @@ from abtem.utils import ind2sub
 from abtem.waves import ProbeWaves
 from abtem.potentials import import_potential
 
-size = 4
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+print(rank)
+
 partitions = (2, 2)
 
-for rank in range(size):
-    assigned_partition = ind2sub(partitions, rank)
+assigned_partition = ind2sub(partitions, rank)
 
-    potential = import_potential('potential.npz')
-    extent = potential.extent
+potential = import_potential('potential.npz')
+extent = potential.extent
 
-    probe = ProbeWaves(energy=80e3, cutoff=.02)
+probe = ProbeWaves(energy=80e3, cutoff=.02)
 
-    scan = GridScan((0., 0), (extent[0] / 2, extent[1]), sampling=.1, endpoint=False)
-    scans = scan.partition(partitions)
+scan = GridScan((0., 0), (extent[0] / 2, extent[1]), sampling=.1, endpoint=False)
+scans = scan.partition(partitions)
 
-    detector = RingDetector(inner=.04, outer=.2)
-    measurements = probe.scan(scan=scans[assigned_partition], potential=potential, detectors=detector, max_batch=50, )
+detector = RingDetector(inner=.04, outer=.2)
+measurements = probe.scan(scan=scans[assigned_partition], potential=potential, detectors=detector, max_batch=50, )
 
-    np.save('export/mpi-partition-{}-{}.npy'.format(*assigned_partition), measurements[detector])
+np.save('export/mpi-partition-{}-{}.npy'.format(*assigned_partition), measurements[detector])
