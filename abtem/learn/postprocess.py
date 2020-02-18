5ced2d6237f33118e1fe81155201c87345d190b7
Jacob Madsen <jacob.jma@gmail.com>
2020-02-18 14:10:41 +0100

    added nms model

--- abtem/learn/postprocess.py
+++ abtem/learn/postprocess.py
@@ -2,53 +2,61 @@ import numpy as np
 from abtem.utils import coordinates_in_disc, ind2sub2d
 
 
-def non_maximum_suppresion(density, distance, threshold, max_num_maxima=np.inf, segmentation=None):
-    shape = density.shape
-    density = density.reshape((-1,))
-    accepted = np.zeros(density.shape, dtype=np.bool_)
+class NonMaximumSuppression:
 
-    suppressed = np.zeros(np.prod(shape), dtype=np.bool_)
+    def __init__(self, distance, threshold, max_num_maxima):
+        self._threshold = threshold
+        self._max_num_maxima = max_num_maxima
+        self._disc = coordinates_in_disc(distance)
 
-    if segmentation is not None:
-        segmentation = segmentation.reshape((segmentation.shape[0],) + (-1,))
-        probabilities = np.zeros(segmentation.shape, dtype=segmentation.dtype)
-    else:
-        probabilities = None
+    def predict(self, density, segmentation=None):
 
-    disc_row, disc_col = coordinates_in_disc(distance)
-    disc_flat = disc_col + disc_row * shape[1]
+        shape = density.shape
+        density = density.reshape((-1,))
+        accepted = np.zeros(density.shape, dtype=np.bool_)
 
-    suppressed[density < threshold] = True
+        suppressed = np.zeros(np.prod(shape), dtype=np.bool_)
 
-    num_maxima = 0
-    for i in np.argsort(-density):
-        if num_maxima == max_num_maxima:
-            break
+        if segmentation is not None:
+            segmentation = segmentation.reshape((segmentation.shape[0],) + (-1,))
+            probabilities = np.zeros(segmentation.shape, dtype=segmentation.dtype)
+        else:
+            probabilities = None
 
-        if not suppressed[i]:
-            accepted[i] = True
+        disc_row, disc_col = self._disc
+        disc_flat = disc_col + disc_row * shape[1]
 
-            row, col = np.unravel_index(i, shape)
+        suppressed[density < self._threshold] = True
 
-            neighbors_row = row + disc_row
-            neighbors_col = col + disc_col
+        num_maxima = 0
+        for i in np.argsort(-density):
+            if num_maxima == self._max_num_maxima:
+                break
 
-            valid = ((neighbors_row > -1) & (neighbors_col > -1) &
-                     (neighbors_row < shape[0]) & (neighbors_col < shape[1]))
+            if not suppressed[i]:
+                accepted[i] = True
 
-            neighbors = i + disc_flat
-            neighbors = neighbors[valid]
-            suppressed[neighbors] = True
+                row, col = np.unravel_index(i, shape)
 
-            if probabilities is not None:
-                probabilities[:, i] = np.sum(segmentation[:, neighbors] * density[neighbors], axis=1)
-                probabilities[:, i] /= np.sum(probabilities[:, i])
+                neighbors_row = row + disc_row
+                neighbors_col = col + disc_col
 
-            num_maxima += 1
+                valid = ((neighbors_row > -1) & (neighbors_col > -1) &
+                         (neighbors_row < shape[0]) & (neighbors_col < shape[1]))
 
-    positions = np.array(np.where(accepted.reshape(shape))).T
-    if segmentation is None:
-        return positions
+                neighbors = i + disc_flat
+                neighbors = neighbors[valid]
+                suppressed[neighbors] = True
 
-    else:
-        return positions, probabilities.reshape((-1,) + shape)[:, positions[:, 0], positions[:, 1]]
+                if probabilities is not None:
+                    probabilities[:, i] = np.sum(segmentation[:, neighbors] * density[neighbors], axis=1)
+                    probabilities[:, i] /= np.sum(probabilities[:, i])
+
+                num_maxima += 1
+
+        positions = np.array(np.where(accepted.reshape(shape))).T
+        if segmentation is None:
+            return positions
+
+        else:
+            return positions, probabilities.reshape((-1,) + shape)[:, positions[:, 0], positions[:, 1]]
