58a059753907075ace85cba851f5eef72b010c84
Jacob Madsen <jacob.jma@gmail.com>
2020-08-03 19:48:00 +0200

    added autodoc typehints

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'nbsphinx',
+    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
