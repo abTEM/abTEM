20b67734fe0424de2f80d31d793714311976cb33
jacob <jacob.jma@gmail.com>
2020-08-16 12:16:03 +0200

    Added mark for testing gpu

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,19 @@
 import pytest
 
 
+def pytest_configure(config):
+    config.addinivalue_line("markers", "gpaw")
+    config.addinivalue_line("markers", "gpu")
+
+
 def pytest_addoption(parser):
     parser.addoption("--rungpaw", action="store_true", help="run gpaw tests")
+    parser.addoption("--rungpu", action="store_true", help="run gpu tests")
 
 
 def pytest_runtest_setup(item):
     if 'gpaw' in item.keywords and not item.config.getvalue("rungpaw"):
         pytest.skip("need --rungpaw option to run")
+
+    if 'gpu' in item.keywords and not item.config.getvalue("rungpu"):
+        pytest.skip("need --rungpu option to run")
