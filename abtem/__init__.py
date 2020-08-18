ffe79ae1aea8d94f7e5280172978baf51cecf84b
jacob <jacob.jma@gmail.com>
2020-08-18 11:22:38 +0200

    Added Measurement class to __init__.py

--- abtem/__init__.py
+++ abtem/__init__.py
@@ -1,8 +1,9 @@
 from abtem import waves
 from abtem.detect import AnnularDetector, FlexibleAnnularDetector, SegmentedDetector, PixelatedDetector
 from abtem.plot import show_atoms
-from abtem.potentials import Potential, ArrayPotential
+from abtem.potentials import Potential, PotentialArray
 from abtem.scan import GridScan, LineScan
 from abtem.temperature import AbstractFrozenPhonons, FrozenPhonons
 from abtem.transfer import CTF
-from abtem.waves import PlaneWave, Probe, SMatrixBuilder, Waves
+from abtem.waves import PlaneWave, Probe, SMatrix, Waves
+from abtem.measure import Measurement
