8e2c7e21be4a9605b786982fb7d57e81a53a5fac
jacob <jacob.jma@gmail.com>
2020-08-20 10:12:05 +0200

    Docstrings for scan.py

--- abtem/scan.py
+++ abtem/scan.py
@@ -1,14 +1,15 @@
 from abc import ABCMeta, abstractmethod
-from typing import Union, Sequence
+from copy import copy
+from typing import Union, Sequence, Tuple
 
 import h5py
 import numpy as np
 from matplotlib.patches import Rectangle
 
 from abtem.base_classes import Grid, HasGridMixin
-from abtem.measure import Calibration
-from abtem.utils import split_integer
 from abtem.device import asnumpy
+from abtem.measure import Calibration, Measurement
+from abtem.utils import split_integer
 
 
 class AbstractScan(metaclass=ABCMeta):
@@ -26,19 +27,36 @@ class AbstractScan(metaclass=ABCMeta):
     @property
     @abstractmethod
     def shape(self) -> tuple:
+        """
+        The shape the scan.
+        """
         pass
 
     @property
     @abstractmethod
     def calibrations(self) -> tuple:
+        """
+        The measurement calibrations associated with the scan.
+        """
         pass
 
     @abstractmethod
     def get_positions(self):
+        """
+        Get the scan positions as numpy array.
+        """
         pass
 
     @abstractmethod
     def insert_new_measurement(self, measurement_key, start, end, new_values):
+        """
+        Insert new measurement values into a Measurement object or HDF5 file.
+
+        :param measurement: Measurement object to insert new values into.
+        :param start: Start of insertion slice.
+        :param end: End of insertion slice.
+        :param new_measurement_values: New measurement values to be inserted. Length should be (end - start).
+        """
         pass
 
     def generate_positions(self, max_batch):
@@ -64,8 +82,22 @@ class AbstractScan(metaclass=ABCMeta):
     def __copy__(self):
         pass
 
+    def copy(self):
+        """
+        Make a copy.
+        """
+        return copy(self)
+
 
 class PositionScan(AbstractScan):
+    """
+    Position scan object.
+
+    Defines a scan based on user-defined positions.
+
+    :param positions: The scan positions [Å].
+    """
+
     def __init__(self, positions):
         self._positions = positions
         super().__init__()
@@ -94,10 +126,22 @@ class PositionScan(AbstractScan):
 
 
 class LineScan(AbstractScan, HasGridMixin):
+    """
+    Line scan object.
+
+    Defines the scan along a straight line.
+
+    :param start: Start point of the scan [Å].
+    :param end: End point of the scan [Å].
+    :param gpts: Number of scan positions.
+    :param sampling: Sampling rate of scan positions [1 / Å].
+    :param endpoint: If True, end is the last position. Otherwise, it is not included. Default is True.
+    """
 
     def __init__(self, start: Sequence[float], end: Sequence[float],
                  gpts: Union[int, Sequence[int]] = None,
-                 sampling: Union[float, Sequence[float]] = None, endpoint: bool = True):
+                 sampling: Union[float, Sequence[float]] = None,
+                 endpoint: bool = True):
 
         super().__init__()
 
@@ -120,45 +164,55 @@ class LineScan(AbstractScan, HasGridMixin):
         return direction, extent
 
     @property
-    def num_measurements(self):
-        return self.gpts[0]
-
-    @property
-    def shape(self):
+    def shape(self) -> Tuple[int]:
         return (self.gpts[0],)
 
     @property
-    def calibrations(self) -> tuple:
+    def calibrations(self) -> Tuple[Calibration]:
         return (Calibration(offset=0, sampling=self.sampling[0], units='Å', name='x'),)
 
     @property
     def start(self) -> np.ndarray:
+        """
+        Start point of the scan [Å].
+        """
         return self._start
 
     @start.setter
-    def start(self, start: np.ndarray):
+    def start(self, start: Sequence[float]):
         self._start = np.array(start)
         self._direction, self.extent = self._direction_and_extent(self._start, self.end)
 
     @property
     def end(self) -> np.ndarray:
+        """
+        End point of the scan [Å].
+        """
         return self.start + self.direction * self.extent
 
     @end.setter
-    def end(self, end: np.ndarray):
+    def end(self, end: Sequence[float]):
         self._direction, self.extent = self._direction_and_extent(self.start, end)
 
     @property
     def direction(self) -> np.ndarray:
+        """
+        Direction of the scan line.
+        """
         return self._direction
 
-    def insert_new_measurement(self, measurement, start, end, new_measurement):
+    def insert_new_measurement(self,
+                               measurement: Measurement,
+                               start: int,
+                               end: int,
+                               new_measurement_values: np.ndarray):
+
         if isinstance(measurement, str):
             with h5py.File(measurement, 'a') as f:
-                f['array'][start:end] += asnumpy(new_measurement)
+                f['array'][start:end] += asnumpy(new_measurement_values)
 
         else:
-            measurement.array[start:end] += asnumpy(new_measurement)
+            measurement.array[start:end] += asnumpy(new_measurement_values)
 
     def get_positions(self) -> np.ndarray:
         x = np.linspace(self.start[0], self.start[0] + np.array(self.extent) * self.direction[0], self.gpts[0],
@@ -167,14 +221,22 @@ class LineScan(AbstractScan, HasGridMixin):
                         endpoint=self.grid.endpoint)
         return np.stack((np.reshape(x, (-1,)), np.reshape(y, (-1,))), axis=1)
 
-    def add_to_mpl_plot(self, ax, linestyle='-', color='r', **kwargs):
+    def add_to_mpl_plot(self, ax, linestyle: str = '-', color: str = 'r', **kwargs):
+        """
+        Add a visualization of the scan line to a matplotlib plot.
+
+        :param ax: The axes of the matplotlib plot the visualization should be added to.
+        :param linestyle: Linestyle of scan line.
+        :param color: Color of the scan line.
+        :param kwargs: Additional options for matplotlib.pyplot.plot as keyword arguments.
+        """
         ax.plot([self.start[0], self.end[0]], [self.start[1], self.end[1]], linestyle=linestyle, color=color, **kwargs)
 
     def __copy__(self):
         return self.__class__(start=self.start, end=self.end, gpts=self.gpts, endpoint=self.grid.endpoint)
 
 
-def unravel_slice_2d(start, end, shape):
+def unravel_slice_2d(start: int, end: int, shape: Tuple[int, int]):
     slices = []
     rows = []
     slices_1d = []
@@ -197,8 +259,20 @@ def unravel_slice_2d(start, end, shape):
 
 
 class GridScan(AbstractScan, HasGridMixin):
+    """
+    Grid scan object.
+
+    Defines the scan on a regular grid.
+
+    :param start: Start corner of the scan [Å].
+    :param end: End corner of the scan [Å].
+    :param gpts: Number of scan positions in the x- and y-direction of the scan.
+    :param sampling: Sampling rate of scan positions [1 / Å].
+    :param endpoint: If True, end is the last position. Otherwise, it is not included. Default is True.
+    """
 
     def __init__(self, start, end, gpts=None, sampling=None, endpoint=False):
+
         super().__init__()
 
         self._start = np.array(start)
@@ -211,10 +285,6 @@ class GridScan(AbstractScan, HasGridMixin):
             raise RuntimeError('Grid gpts or sampling must be set')
 
         self._grid = Grid(extent=end - start, gpts=gpts, sampling=sampling, dimensions=2, endpoint=endpoint)
-
-    @property
-    def num_measurements(self):
-        return np.prod(self.gpts)
 
     @property
     def shape(self):
