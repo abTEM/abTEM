a9dc26062084a9b861e8434fe7bab7484687fd16
Toma Susi <toma.susi@iki.fi>
2020-08-21 11:59:47 +0200

    Improved docstrings.

--- abtem/noise.py
+++ abtem/noise.py
@@ -1,9 +1,25 @@
+"""Module for describing different kinds of noise."""
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 from copy import copy
 
 
 def _pixel_times(dwell_time, flyback_time, shape):
+    """
+    Pixel times internal function
+
+    Function for calculating scan pixel times.
+
+    Parameters
+    ----------
+    dwell_time : float
+        Dwell time on a single pixel in s.
+    flyback_time : float
+        Flyback time for the scanning probe at the end of each scan line in s.
+    shape : two ints
+        Dimensions of a scan in pixels.
+    """
+
     line_time = (dwell_time * shape[1]) + flyback_time
     slow_time = np.tile(np.linspace(line_time, shape[0] * line_time, shape[0])[:, None], (1, shape[1]))
     fast_time = np.tile(np.linspace((line_time - flyback_time) / shape[1],
@@ -12,6 +28,21 @@ def _pixel_times(dwell_time, flyback_time, shape):
 
 
 def _single_axis_distortion(time, max_frequency, num_components):
+    """
+    Single axis distortion internal function
+
+    Function for emulating a scan distortion along a single axis.
+
+    Parameters
+    ----------
+    time : float
+        Time constant for the distortion in s.
+    max_frequency : float
+        Maximum noise frequency in 1 / s.
+    num_components: int
+        Number of frequency components.
+    """
+
     frequencies = np.random.rand(num_components, 1, 1) * max_frequency
     amplitudes = np.random.rand(num_components, 1, 1) / np.sqrt(frequencies)
     displacements = np.random.rand(num_components, 1, 1) / frequencies
@@ -19,6 +50,23 @@ def _single_axis_distortion(time, max_frequency, num_components):
 
 
 def _make_displacement_field(time, max_frequency, num_components, rms_power):
+    """
+    Displacement field creation internal function
+
+    Function to create a displacement field to emulate 2D scan distortion.
+
+    Parameters
+    ----------
+    time : float
+       Time constant for the distortion in s.
+    max_frequency : float
+       Maximum noise frequency in 1 / s.
+    num_components : int
+       Number of frequency components.
+    rms_power : float
+       Root-mean-square power of the distortion.
+    """
+
     profile_x = _single_axis_distortion(time, max_frequency, num_components)
     profile_y = _single_axis_distortion(time, max_frequency, num_components)
 
@@ -28,13 +76,28 @@ def _make_displacement_field(time, max_frequency, num_components, rms_power):
     frame_mag_deviation = (1 + x_mag_deviation) * (1 + y_mag_deviation) - 1
     frame_mag_deviation = np.sqrt(np.mean(frame_mag_deviation ** 2))
 
-    profile_x *= rms_power / (235.5 * frame_mag_deviation)
+    profile_x *= rms_power / (235.5 * frame_mag_deviation) #JM please explain where 235.5 comes from.
     profile_y *= rms_power / (235.5 * frame_mag_deviation)
 
     return profile_x, profile_y
 
 
 def _apply_displacement_field(image, distortion_x, distortion_y):
+    """
+    Displacement field applying function
+
+    Function to apply a displacement field to an image.
+
+    Parameters
+    ----------
+    image : ndarray
+        Image array.
+    distortion_x : ndarray
+        Displacement field along the x axis.
+    distortion_y : ndarray
+        Displacement field along the y axis.
+    """
+
     x = np.arange(0, image.shape[0])
     y = np.arange(0, image.shape[1])
 
@@ -51,6 +114,27 @@ def _apply_displacement_field(image, distortion_x, distortion_y):
 
 
 def add_scan_noise(image, dwell_time, flyback_time, max_frequency, rms_power, num_components=200):
+    """
+    Scan noise function
+
+    Function to add scan noise to an image.
+
+    Parameters
+    ----------
+    image : ndarray
+        Image array.
+    dwell_time : float
+        Dwell time on a single pixel in s.
+    flyback_time : float
+        Flyback time for the scanning probe at the end of each scan line in s.
+    max_frequency : float
+        Maximum noise frequency in 1 / s.
+    rms_power : float
+        Root-mean-square power of the distortion.
+    num_components : int, optional
+        Number of frequency components.
+    """
+
     image = image.copy()
     time = _pixel_times(dwell_time, flyback_time, image.array.T.shape)
     displacement_x, displacement_y = _make_displacement_field(time, max_frequency, num_components, rms_power)
@@ -60,6 +144,7 @@ def add_scan_noise(image, dwell_time, flyback_time, max_frequency, rms_power, nu
 
 
 def poisson_noise(measurement, dose):
+    """Function to describe Poisson noise for a given measurement with an irradiation dose given in electrons / Å^2."""
     pixel_area = np.product([calibration.sampling for calibration in measurement.calibrations])
     new_copy = copy(measurement)
     array = new_copy.array
