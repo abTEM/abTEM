933b15e085a7e2636bb242621084c5a8630337f5
jacob <jacob.jma@gmail.com>
2020-08-23 14:38:28 +0200

    Support for individually specified vibration standard displacements and displacement directions

--- abtem/temperature.py
+++ abtem/temperature.py
@@ -74,28 +74,64 @@ class FrozenPhonons(AbstractFrozenPhonons):
         Atoms with the average atomic configuration.
     num_configs: int
         Number of frozen phonon configurations.
-    sigmas: dict
-        Mapping from atomic species to the standard deviation of the displacements of that atomic species.
-        The atomic species can be specified as atomic number or symbol.
+    sigmas: float or dict or list
+        If float, the standard deviation of the displacements is assumed to be identical for all atoms.
+        If dict, a displacement standard deviation should be provided for each species. The atomic species can be
+        specified as atomic number or symbol.
+        If list or array, a displacement standard deviation should be provided for each atom.
+    direction: str
+        The displacement directions of the atoms as a string; for example 'xy' for displacement in the x- and
+        y-direction.
     seed: int
         Seed for random number generator.
     """
 
-    def __init__(self, atoms: Atoms, num_configs: int, sigmas: Mapping[Union[str, int], float], seed=None):
+    def __init__(self,
+                 atoms: Atoms,
+                 num_configs: int,
+                 sigmas: Union[float, Mapping[Union[str, int], float], Sequence[float]],
+                 directions: str = 'xy',
+                 seed=None):
 
-        new_sigmas = {}
-        for key, sigma in sigmas.items():
-            try:
-                new_sigmas[atomic_numbers[key]] = sigma
-            except KeyError:
-                pass
+        try:
+            sigmas_array = np.array(sigmas, dtype=np.float)
 
-        unique_atomic_numbers = np.unique(atoms.get_atomic_numbers())
+            if len(sigmas_array) == 1:
+                sigmas_array = np.tile(sigmas_array)
+            elif len(sigmas_array) != len(atoms):
+                raise RuntimeError('The displacement standard deviation must be provided for all atoms.')
 
-        if len(set(unique_atomic_numbers).intersection(set(new_sigmas.keys()))) != len(unique_atomic_numbers):
-            raise RuntimeError('Displacement standard deviation not provided for all atomic species.')
+        except:
+            sigmas_array = np.zeros(len(atoms), dtype=np.float)
+            new_sigmas = {}
+            for key, sigma in sigmas.items():
+                try:
+                    new_sigmas[atomic_numbers[key]] = sigma
+                except KeyError:
+                    pass
 
-        self._sigmas = new_sigmas
+            for i, atom in enumerate(atoms):
+                try:
+                    sigmas_array[i] = new_sigmas[atom.number]
+                except KeyError:
+                    raise RuntimeError('Displacement standard deviation not provided for all atomic species.')
+
+        self._sigmas = sigmas_array
+
+        directions = list(set(directions.lower()))
+
+        new_directions = []
+        for direction in directions:
+            if direction == 'x':
+                new_directions += [0]
+            elif direction == 'y':
+                new_directions += [1]
+            elif direction == 'z':
+                new_directions += [2]
+            else:
+                raise RuntimeError('Directions must be "x", "y" or "z" not {}.')
+
+        self._directions = new_directions
         self._atoms = atoms
         self._num_configs = num_configs
         self._seed = seed
@@ -115,9 +151,8 @@ class FrozenPhonons(AbstractFrozenPhonons):
             atoms = self._atoms.copy()
             positions = atoms.get_positions()
 
-            for number, sigma in self._sigmas.items():
-                indices = np.where(atoms.get_atomic_numbers() == number)[0]
-                positions[indices] += sigma * np.random.randn(len(indices), 3)
+            for direction in self._directions:
+                positions[:, direction] += self._sigmas * np.random.randn(len(positions))
 
             atoms.set_positions(positions)
             atoms.wrap()
