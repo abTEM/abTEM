fixed issue with tanh sinh error estimation that was slowing down DFT potential calculation

abtem/dft.py
@@ -233,7 +233,7 @@ class GPAWPotential(AbstractPotentialBuilder):
 
                 slice_atoms = pad_atoms(slice_atoms, cutoff)
 
-                R = np.geomspace(1e-8, cutoff, int(np.ceil(cutoff / np.min(self.sampling))) * 10)
+                R = np.geomspace(np.min(self.sampling)/2, cutoff, int(np.ceil(cutoff / np.min(self.sampling))) * 10)
 
                 vr = np.zeros((len(slice_atoms), len(R)), np.float32)
                 dvdr = np.zeros((len(slice_atoms), len(R)), np.float32)
@@ -242,8 +242,7 @@ class GPAWPotential(AbstractPotentialBuilder):
 
                     f = interp1d(r * units.Bohr, v, fill_value=(v[0], 0), bounds_error=False, kind='linear')
 
-                    integrator = PotentialIntegrator(f, R, self.get_slice_thickness(i))
-                    #am, bm = a - atom.z, b - atom.z
+                    integrator = PotentialIntegrator(f, R, self.get_slice_thickness(i), tolerance=1e-6)
 
                     vr[j], dvdr[j] = integrator.integrate(np.array([atom.z]), a, b, xp=np)
 
@@ -251,7 +250,7 @@ class GPAWPotential(AbstractPotentialBuilder):
                 sampling = np.asarray(self.sampling, dtype=np.float32)
                 run_length_enconding = np.zeros((2,), dtype=np.int32)
                 run_length_enconding[1] = len(slice_atoms)
-                
+
                 interpolate_radial_functions(array,
                                              run_length_enconding,
                                              disc_indices,

abtem/potentials.py
@@ -307,8 +307,6 @@ class PotentialIntegrator:
             return self._function(np.sqrt(self.r[0] ** 2 + (z * max_interval / 2 + max_interval / 2) ** 2))
 
         value, error_estimate, step_size, order = integrate(f, -1, 1, self._tolerance)
-        step_size = step_size
-        order = order
 
         self._xk, self._wk = tanh_sinh_nodes_and_weights(step_size, order)
 
@@ -650,7 +648,8 @@ class Potential(AbstractTDSPotentialBuilder, HasDeviceMixin):
             cutoff = self.get_cutoff(number)
             soft_function = self.get_tapered_function(number)
             inner_cutoff = np.min(self.sampling) / 2.
-            num_points = int(np.ceil(cutoff / np.min(self.sampling) * 2.))
+
+            num_points = int(np.ceil(cutoff / np.min(self.sampling) * 10.))
             r = np.geomspace(inner_cutoff, cutoff, num_points)
             max_interval = self.slice_thickness
             self._integrators[number] = PotentialIntegrator(soft_function, r, max_interval, cutoff)

abtem/tanh_sinh.py
@@ -35,15 +35,21 @@ def _error_estimate(eps: float,
         error_estimate = 0
 
     else:
-        e1 = abs(value_estimates[-1] - value_estimates[-2])
+        #e1 = abs(value_estimates[-1] - value_estimates[-2])
         # e2 = abs(value_estimates[-1] - value_estimates[-3])
         # e3 = eps * max(max(abs(left_summands)), max(abs(right_summands)))
-        e4 = max(abs(left_summands[-1]), abs(right_summands[-1]))
+        #e4 = max(abs(left_summands[-1]), abs(right_summands[-1]))
         # n = np.float((np.log(e1 + np.finfo(e1).eps) / (np.log(e2 + np.finfo(e2).eps))))
+
+        e1 = abs(value_estimates[-1] - value_estimates[-2])
+        e2 = abs(value_estimates[-1] - value_estimates[-3])
+        e3 = eps * max(max(abs(left_summands)), max(abs(right_summands)))
+        e4 = max(abs(left_summands[-1]), abs(right_summands[-1]))
+        error_estimate = max(e1 ** (np.log(e1) / np.log(e2)), e1 ** 2, e3, e4)
 
         # with np.errstate(over='ignore'):
         #    error_estimate = max(e1 ** n, e1 ** 2, e3, e4)
-        error_estimate = max(e1, e4)
+        #error_estimate = max(e1, e4)
 
     return error_estimate
 
@@ -143,6 +149,7 @@ def integrate(f: Callable, a: float, b: float, eps: float, max_steps: int = 20):
             value_estimates.append(value_estimates[-1] / 2 + np.sum(left_summands) + np.sum(right_summands))
 
         error_estimate = _error_estimate(eps, value_estimates, left_summands, right_summands)
+
         if abs(error_estimate) < eps:
             success = True
             break