Fixed bug in icom calculation

abtem/measure.py
@@ -753,8 +753,8 @@ def intgrad2d(gradient, sampling=None):
     """
     gx, gy = gradient
     (ny, nx) = gx.shape
-    ikx = np.fft.fftfreq(nx, 1, d=sampling)
-    iky = np.fft.fftfreq(ny, 1, d=sampling)
+    ikx = np.fft.fftfreq(nx, d=sampling)
+    iky = np.fft.fftfreq(ny, d=sampling)
     grid_ikx, grid_iky = np.meshgrid(ikx, iky)
     k = grid_ikx ** 2 + grid_iky ** 2
     k[k == 0] = 1e-12