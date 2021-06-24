99b26c58aaef3d600cabc2e12af4ae48b7863aca
jacob <jacob.jma@gmail.com>
2021-06-24 11:21:26 +0200

    WIP: ensuring cupy is optional in dask branch

--- abtem/basic/complex.py
+++ abtem/basic/complex.py
@@ -1,17 +1,11 @@
+import cupy as cp
+import dask.array as da
 import numba as nb
 import numpy as np
-import dask.array as da
-import cupy as cp
+
+from abtem.basic.backend import check_cupy_is_installed
 
 
-# @nb.guvectorize([(nb.float32[:, :], nb.complex64[:, :])], '(n,m)->(n,m)')
-# def _complex_exponential(x, out):
-#     """
-#     Calculate the complex exponential.
-#     """
-#     for i in range(x.shape[0]):
-#         for j in range(x.shape[1]):
-#             out[i, j] = np.cos(x[i, j]) + 1.j * np.sin(x[i, j])
 @nb.vectorize([nb.complex64(nb.float32), nb.complex128(nb.float64)])
 def _complex_exponential(x):
     """
@@ -19,15 +13,6 @@ def _complex_exponential(x):
     """
     return np.cos(x) + 1.j * np.sin(x)
 
-
-# @nb.guvectorize([(nb.complex64[:, :], nb.float32[:, :])], '(n,m)->(n,m)')
-# def _abs2(x, out):
-#     """
-#     Calculate the absolute square of a complex number.
-#     """
-#     for i in range(x.shape[0]):
-#         for j in range(x.shape[1]):
-#             out[i, j] = x[i, j].real ** 2 + x[i, j].imag ** 2
 
 @nb.vectorize([nb.float32(nb.complex64), nb.float64(nb.complex128)])
 def _abs2(x):
@@ -44,6 +29,8 @@ def abs2(x, **kwargs):
     if isinstance(x, da.core.Array):
         return x.map_blocks(_abs2, **kwargs)
 
+    check_cupy_is_installed()
+
     if isinstance(x, cp.ndarray):
         return cp.abs(x) ** 2
 
@@ -56,6 +43,8 @@ def complex_exponential(x, **kwargs):
 
     if isinstance(x, da.core.Array):
         return x.map_blocks(_complex_exponential)
+
+    check_cupy_is_installed()
 
     if isinstance(x, cp.ndarray):
         return cp.exp(1.j * x)
