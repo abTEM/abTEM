f13708433a0f0ad8873713c8ab2e585c34a6218b
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-01-18 07:49:43 -0700

    Import from collections.abc rather than collections

--- abtem/temperature.py
+++ abtem/temperature.py
@@ -2,7 +2,7 @@
 from abc import abstractmethod, ABCMeta
 from typing import Mapping, Union, Sequence
 from numbers import Number
-from collections import Iterable
+from collections.abc import Iterable
 
 import numpy as np
 from ase import Atoms
