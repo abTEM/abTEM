34095f56e9d65d2e8e1881e3ab79599a80b8c392
jacobmadsen <jacob.jma@gmail.com>
2022-08-12 22:26:21 +0200

    transition from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,44 +1,4 @@
-import os
-from io import open as io_open
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-__version__ = None
-src_dir = os.path.abspath(os.path.dirname(__file__))
-version_file = os.path.join(src_dir, 'abtem', '_version.py')
-with io_open(version_file, mode='r') as fd:
-    exec(fd.read())
-
-fndoc = os.path.join(src_dir, 'README.md')
-with io_open(fndoc, mode='r', encoding='utf-8') as fd:
-    README_md = fd.read()
-
-setup(
-    name='abtem',
-    version=__version__,
-    description='ab initio Transmission Electron Microscopy',
-    long_description=README_md,
-    url='https://github.com/jacobjma/abtem',
-    author='Jacob Madsen',
-    author_email='jacob.madsen@univie.ac.at',
-    maintainer='Jacob Madsen',
-    maintainer_email='jacob.madsen@univie.ac.at',
-    platforms=['any'],
-    python_requires='>=3.6',
-    install_requires=[
-        'numpy',
-        'pyfftw',
-        'scipy',
-        'numba',
-        'imageio',
-        'dask',
-        'zarr',
-        'dask',
-        'matplotlib',
-        'ase',
-        'tqdm',
-        'psutil'],
-    tests_require=['pytest'],
-    packages=find_packages(),
-    include_package_data=True,
-)
+if __name__ == "__main__":
+    setup()
