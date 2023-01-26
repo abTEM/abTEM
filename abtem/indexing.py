ensure correct normalization of plane waves after taking diffraction patterns

abtem/indexing.py
@@ -338,14 +338,15 @@ class IndexedDiffractionPatterns:
     def vectors(self) -> np.ndarray:
         return self._vectors
 
-    def remove_equivalent(self, divide_threshold: float = 1.0) -> "IndexedDiffractionPatterns":
+    def remove_equivalent(self, inequivalency_threshold: float = 1.0) -> "IndexedDiffractionPatterns":
         """
         Remove symmetry equivalent diffraction spots.
 
         Parameters
         ----------
-        divide_threshold : float
-            Keep symmetry equivalent spots with a relative intensity difference larger than this.
+        inequivalency_threshold : float
+            Relative intensity difference to determine whether two symmetry-equivalent diffraction spots should be
+            independently labeled (e.g. due to a unit cell with a basis of more than one element).
         """
         miller_indices, intensities = self._dict_to_arrays(self._spots)
 
@@ -357,7 +358,7 @@ class IndexedDiffractionPatterns:
         include = _find_equivalent_spots(
             miller_indices,
             intensities=summed_intensities,
-            intensity_split=divide_threshold,
+            intensity_split=inequivalency_threshold,
         )
 
         miller_indices, intensities = miller_indices[include], intensities[include]
@@ -386,7 +387,7 @@ class IndexedDiffractionPatterns:
         Parameters
         ----------
         threshold : float
-            Relative intensity of threshold for removing diffraction spots.
+            Relative intensity threshold for removing diffraction spots.
 
         """
 
@@ -451,29 +452,32 @@ class IndexedDiffractionPatterns:
 
     def to_dataframe(
             self,
-            intensity_threshold: float = 1e-2,
-            divide_threshold: float = 1.0,
+            intensity_threshold: float = 1e-3,
+            inequivalency_threshold: float = 1.0,
             normalize: bool = False,
-            index: Union[str, int] = 0,
     ):
         """
         Convert the indexed diffraction to pandas dataframe.
 
         Parameters
         ----------
-        intensity_threshold :
-        divide_threshold :
-        normalize :
-        index :
+        intensity_threshold : float
+            Relative intensity threshold for removing diffraction spots from the dataframe.
+        inequivalency_threshold : float
+            Relative intensity difference to determine whether two symmetry-equivalent diffraction spots should be
+            independently labeled (e.g. due to a unit cell with a basis of more than one element).
+        normalize : bool
+            If True, normalize
 
         Returns
         -------
+        dataframe_with_spots : pd.DataFrame
 
         """
 
         import pandas as pd
 
-        indexed = self.remove_equivalent(divide_threshold=divide_threshold)
+        indexed = self.remove_equivalent(inequivalency_threshold=inequivalency_threshold)
 
         indexed = indexed.remove_low_intensity(intensity_threshold)
 
@@ -491,10 +495,22 @@ class IndexedDiffractionPatterns:
         try:
             return pd.DataFrame(spots)
         except ValueError:
-            return pd.DataFrame(spots, index=[index])
+            return pd.DataFrame(spots, index=[0])
 
     def show(self, **kwargs):
-        from abtem.visualize import plot_diffraction_pattern
+        """
+
+
+        Parameters
+        ----------
+        kwargs
+
+        Returns
+        -------
+
+        """
+
+        from abtem.visualize import _show_indexed_diffraction_pattern
 
         indexed_diffraction_patterns = self
         if self.ensemble_shape:
@@ -502,4 +518,4 @@ class IndexedDiffractionPatterns:
                 (0,) * len(self.ensemble_shape)
                 ]
 
-        return plot_diffraction_pattern(indexed_diffraction_patterns, **kwargs)
+        return _show_indexed_diffraction_pattern(indexed_diffraction_patterns, **kwargs)

abtem/measurements.py
@@ -26,7 +26,11 @@ from abtem.core.backend import cp, get_array_module, get_ndimage_module
 from abtem.core.complex import abs2
 from abtem.core.energy import energy2wavelength
 from abtem.core.fft import fft_interpolate, fft_crop
-from abtem.core.grid import adjusted_gpts, polar_spatial_frequencies, spatial_frequencies
+from abtem.core.grid import (
+    adjusted_gpts,
+    polar_spatial_frequencies,
+    spatial_frequencies,
+)
 from abtem.indexing import IndexedDiffractionPatterns
 from abtem.core.interpolate import interpolate_bilinear
 from abtem.core.utils import CopyMixin, EqualityMixin, label_to_index
@@ -208,11 +212,13 @@ def _annular_detector_mask(
     xp=np,
 ) -> Union[np.ndarray, List[np.ndarray]]:
 
-    kx, ky = spatial_frequencies(gpts, (1 / sampling[0] / gpts[0], 1 / sampling[1] / gpts[1]), False, xp)
+    kx, ky = spatial_frequencies(
+        gpts, (1 / sampling[0] / gpts[0], 1 / sampling[1] / gpts[1]), False, xp
+    )
 
     k2 = kx[:, None] ** 2 + ky[None] ** 2
 
-    bins = (k2 >= inner ** 2) & (k2 < outer ** 2)
+    bins = (k2 >= inner**2) & (k2 < outer**2)
 
     if np.any(np.array(offset) != 0.0):
         offset = (
@@ -1965,7 +1971,9 @@ class DiffractionPatterns(BaseMeasurement):
 
         diffraction_patterns = self.block_direct()
         diffraction_patterns = diffraction_patterns.to_cpu()
-        return IndexedDiffractionPatterns.index_diffraction_patterns(diffraction_patterns, cell)
+        return IndexedDiffractionPatterns.index_diffraction_patterns(
+            diffraction_patterns, cell
+        )
 
     @property
     def fftshift(self):
@@ -2267,51 +2275,49 @@ class DiffractionPatterns(BaseMeasurement):
 
         return _gaussian_source_size(self, sigma)
 
-        # if len(_scan_axes(self)) < 2:
-        #     raise RuntimeError(
-        #         "Gaussian source size not implemented for diffraction patterns with less than two scan axes."
-        #     )
-        #
-        # if np.isscalar(sigma):
-        #     sigma = (sigma,) * 2
-        #
-        # xp = get_array_module(self.array)
-        # gaussian_filter = get_ndimage_module(self._array).gaussian_filter
-        #
-        # padded_sigma = ()
-        # depth = ()
-        # i = 0
-        # for axis, n in zip(self.ensemble_axes, self.ensemble_shape):
-        #     if axis in _scan_axes(self):
-        #         scan_sampling = _scan_sampling(self)[i]
-        #         padded_sigma += (sigma[i] / scan_sampling,)
-        #         depth += (min(int(np.ceil(4.0 * sigma[i] / scan_sampling)), n),)
-        #         i += 1
-        #     else:
-        #         padded_sigma += (0.0,)
-        #         depth += (0,)
-        #
-        # padded_sigma += (0.0,) * 2
-        # depth += (0,) * 2
-        #
-        # if self.is_lazy:
-        #     array = self.array.map_overlap(
-        #         gaussian_filter,
-        #         sigma=padded_sigma,
-        #         mode="wrap",
-        #         depth=depth,
-        #         meta=xp.array((), dtype=xp.float32),
-        #     )
-        # else:
-        #     array = gaussian_filter(self.array, sigma=padded_sigma, mode="wrap")
-        #
-        # return self.__class__(
-        #     array,
-        #     sampling=self.sampling,
-        #     ensemble_axes_metadata=self.ensemble_axes_metadata,
-        #     metadata=self.metadata,
-        #     fftshift=self.fftshift,
-        # )
+    def poisson_noise(
+        self,
+        dose_per_area: float = None,
+        total_dose: float = None,
+        samples: int = 1,
+        seed: int = None,
+    ):
+        """
+        Add Poisson noise (i.e. shot noise) to a measurement corresponding to the provided 'total_dose' (per measurement
+        if applied to an ensemble) or 'dose_per_area' (not applicable for single measurements).
+
+        Parameters
+        ----------
+        dose_per_area : float, optional
+            The irradiation dose per unit of scan area [electrons per Å:sup:`2`]. This is only valid if the diffraction
+            patterns has two scan axes.
+        total_dose : float, optional
+            The irradiation dose per diffraction pattern.
+        samples : int, optional
+            The number of samples to draw from a Poisson distribution. If this is greater than 1, an additional
+            ensemble axis will be added to the measurement.
+        seed : int, optional
+            Seed the random number generator.
+
+        Returns
+        -------
+        noisy_measurement : BaseMeasurement
+            The noisy measurement.
+        """
+
+        if len(_scan_shape(self)) < 2 and dose_per_area is not None:
+            raise ValueError(
+                "diffraction patterns has less than two scan axes, provide 'total_dose' not 'dose_per_area' "
+            )
+
+        # TODO: normalization
+
+        return super().poisson_noise(
+            dose_per_area=dose_per_area,
+            total_dose=total_dose,
+            samples=samples,
+            seed=seed,
+        )
 
     def polar_binning(
         self,
@@ -2514,8 +2520,7 @@ class DiffractionPatterns(BaseMeasurement):
             # plt.imshow(xp.asnumpy(bins))
             # plt.show()
 
-
-            #bins = xp.ones_like(array)
+            # bins = xp.ones_like(array)
 
             return xp.sum(array * bins, axis=(-2, -1))
 

abtem/waves.py
@@ -701,7 +701,7 @@ class Waves(HasArray, BaseWaves):
             metadata=metadata,
         )
 
-    def images(self):
+    def complex_images(self):
         """
         The complex array of the wave functions at the image plane.
 
@@ -727,7 +727,6 @@ class Waves(HasArray, BaseWaves):
         max_angle: Union[str, float] = "cutoff",
         gpts: Tuple[int, int] = None,
         normalization: str = "values",
-        overwrite_x: bool = False,
     ) -> "Waves":
         """
         Downsample the wave functions to a lower maximum scattering angle.
@@ -798,6 +797,7 @@ class Waves(HasArray, BaseWaves):
         fftshift: bool = True,
         parity: str = "odd",
         return_complex: bool = False,
+        ensure_reciprocal_space_normalization: bool = True,
     ) -> DiffractionPatterns:
         """
         Calculate the intensity of the wave functions at the diffraction plane.
@@ -827,6 +827,9 @@ class Waves(HasArray, BaseWaves):
         parity : {'same', 'even', 'odd', 'none'}
             The parity of the shape of the diffraction patterns. Default is 'odd', so that the shape of the diffraction
             pattern is odd with the zero at the middle.
+        return_complex : bool
+            If True, return complex-valued diffraction patterns (i.e. the wave function in reciprocal space)
+            (default is False).
 
         Returns
         -------
@@ -834,8 +837,11 @@ class Waves(HasArray, BaseWaves):
             The diffraction pattern(s).
         """
 
-        def _diffraction_pattern(array, new_gpts, return_complex, fftshift):
+        def _diffraction_pattern(array, new_gpts, return_complex, fftshift, normalize):
             xp = get_array_module(array)
+
+            if normalize:
+                array = array / np.prod(array.shape[-2:])
 
             array = fft2(array, overwrite_x=False)
 
@@ -853,6 +859,20 @@ class Waves(HasArray, BaseWaves):
         xp = get_array_module(self.array)
         new_gpts = self._gpts_within_angle(max_angle, parity=parity)
 
+        metadata = copy(self.metadata)
+        metadata["label"] = "intensity"
+        metadata["units"] = "arb. unit"
+
+        if (
+            ensure_reciprocal_space_normalization
+            and "normalization" in metadata
+            and metadata["normalization"] == "values"
+        ):
+            metadata["normalization"] = "reciprocal_space"
+            normalize = True
+        else:
+            normalize = False
+
         validate_gpts(new_gpts)
 
         if self.is_lazy:
@@ -863,6 +883,7 @@ class Waves(HasArray, BaseWaves):
                 new_gpts=new_gpts,
                 fftshift=fftshift,
                 return_complex=return_complex,
+                normalize=normalize,
                 chunks=self.array.chunks[:-2] + ((new_gpts[0],), (new_gpts[1],)),
                 meta=xp.array((), dtype=dtype),
             )
@@ -872,11 +893,8 @@ class Waves(HasArray, BaseWaves):
                 new_gpts=new_gpts,
                 return_complex=return_complex,
                 fftshift=fftshift,
+                normalize=normalize,
             )
-
-        metadata = copy(self.metadata)
-        metadata["label"] = "intensity"
-        metadata["units"] = "arb. unit"
 
         diffraction_patterns = DiffractionPatterns(
             pattern,
@@ -1360,7 +1378,12 @@ class PlaneWave(_WavesFactory):
 
     @property
     def metadata(self):
-        return {"energy": self.energy, **self._tilt.metadata}
+        metadata = {
+            "energy": self.energy,
+            **self._tilt.metadata,
+            "normalization": ("reciprocal_space" if self._normalize else "values"),
+        }
+        return metadata
 
     @property
     def normalize(self):