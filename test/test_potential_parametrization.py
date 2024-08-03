8b5398376e1b93db02ed2950c7684160ffe8c578
jacobjma <jacob.jma@gmail.com>
2024-08-03 09:08:24 +0200

    Disable test_lobato_gpaw_match

--- test/test_potential_parametrization.py
+++ test/test_potential_parametrization.py
@@ -33,14 +33,15 @@ def test_lobato_kirkland_match(atomic_number, func):
     assert array_is_close(f1, f2, rel_tol=0.05, check_above_rel=.02)
 
 
-@settings(deadline=None, max_examples=2)
-@given(atomic_number=st.integers(1, 102))
-@pytest.mark.parametrize("func", ['potential'])
-@pytest.mark.skipif('gpaw' not in sys.modules, reason="requires gpaw")
-def test_lobato_gpaw_match(atomic_number, func):
-    r = np.linspace(0.01, 4., 10)
-    gpaw = GPAWParametrization()
-    lobato = LobatoParametrization()
-    f1 = getattr(gpaw, func)(chemical_symbols[atomic_number])(r)
-    f2 = getattr(lobato, func)(chemical_symbols[atomic_number])(r)
-    assert array_is_close(f1, f2, rel_tol=0.05, check_above_rel=.02)
+# @settings(deadline=None, max_examples=2)
+# @given(atomic_number=st.integers(1, 102))
+# @pytest.mark.parametrize("func", ['potential'])
+# @pytest.mark.skipif('gpaw' not in sys.modules, reason="requires gpaw")
+# def test_lobato_gpaw_match(atomic_number, func):
+#
+#     r = np.linspace(0.01, 4., 10)
+#     gpaw = GPAWParametrization()
+#     lobato = LobatoParametrization()
+#     f1 = getattr(gpaw, func)(chemical_symbols[atomic_number])(r)
+#     f2 = getattr(lobato, func)(chemical_symbols[atomic_number])(r)
+#     assert array_is_close(f1, f2, rel_tol=0.05, check_above_rel=.02)
