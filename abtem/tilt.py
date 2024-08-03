80ccb32a782ed8646acb0d6f833073efb3483e06
jacobjma <jacob.jma@gmail.com>
2024-08-03 09:07:21 +0200

    Update unit of precession angle to radians in docstring

--- abtem/tilt.py
+++ abtem/tilt.py
@@ -68,7 +68,7 @@ def precession_tilts(
     Parameters
     ----------
     precession_angle : float
-        Precession angle [mrad].
+        Precession angle [rad].
     num_samples : int
         Number of tilt samples.
     min_azimuth : float, optional
