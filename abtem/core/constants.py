79afe4d1148c0428c098b6470eafdbe6e7be0a9e
jacobjma <jacob.jma@gmail.com>
2024-08-31 22:02:30 +0200

    Refactor constants.py: Improve code readability and mypy compatibility

--- abtem/core/constants.py
+++ abtem/core/constants.py
@@ -1,9 +1,8 @@
-from ase import units
+from ase import units  # type: ignore
 import numpy as np
 
 # Vacuum permitivity in ASE units
-eps0 = units._eps0 * units.A ** 2 * units.s ** 4 / (units.kg * units.m ** 3)
+eps0 = units._eps0 * units.A**2 * units.s**4 / (units.kg * units.m**3)
 
 # Conversion factor from unitless potential parametrizations to ASE potential units
 kappa = 4 * np.pi * eps0 / (2 * np.pi * units.Bohr * units._e * units.C)
-
