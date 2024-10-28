e79e45afa7ed2fd092256a1dcd66bcffb98b9ea5
jacobjma <jacob.jma@gmail.com>
2024-10-28 19:32:53 +0100

    removed gpaw as in import due to issues with import warnings

--- abtem/__init__.py
+++ abtem/__init__.py
@@ -25,7 +25,6 @@ from abtem.measurements import (
     RealSpaceLineProfiles,
     ReciprocalSpaceLineProfiles,
 )
-from abtem.potentials.charge_density import ChargeDensityPotential
 from abtem.potentials.iam import CrystalPotential, Potential, PotentialArray
 from abtem.prism.s_matrix import SMatrix, SMatrixArray
 from abtem.scan import CustomScan, GridScan, LineScan
@@ -33,21 +32,11 @@ from abtem.transfer import CTF, Aperture, SpatialEnvelope, TemporalEnvelope
 from abtem.visualize.visualizations import show_atoms
 from abtem.waves import PlaneWave, Probe, Waves
 
-
-def _dynamic_import_gpaw():
-    module_name = "abtem.potentials.gpaw"
-    class_name = "GPAWPotential"
-    module = importlib.import_module(module_name)
-    setattr(sys.modules[__name__], class_name, getattr(module, class_name))
-
-_dynamic_import_gpaw()
-
 __all__ = [
     "__version__",
     "distributions",
     "orthogonalize_cell",
     "standardize_cell",
-    "ChargeDensityPotential",
     "axes",
     "config",
     "concatenate",
@@ -58,7 +47,6 @@ __all__ = [
     "FlexibleAnnularDetector",
     "PixelatedDetector",
     "WavesDetector",
-    "GPAWPotential",
     "Images",
     "DiffractionPatterns",
     "RealSpaceLineProfiles",
